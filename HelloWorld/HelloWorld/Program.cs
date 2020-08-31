186b16adf7d66ea6f22a81ce7f69ef9511dbdc9d
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2020-08-31 09:55:43 +0200

    age and name

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace HelloWorld
 {
@@ -7,6 +8,33 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+
+            Console.WriteLine("Vad heter du?");
+            String namn = Console.ReadLine();
+
+            Console.WriteLine("Vad är ditt ålder?");
+            int age = Console.ReadLine();
+
+            Console.WriteLine("Lever du?");
+            String svar = Console.ReadLine();
+
+            String svar1 = "ja";
+            String svar2 = "nej";
+
+            if (svar == svar1)
+            {
+                bool svaret = true;
+
+            }
+
+            if (svar == svar2)
+            {
+                bool svaret = false;
+            }
+
+
+
+
         }
     }
 }
